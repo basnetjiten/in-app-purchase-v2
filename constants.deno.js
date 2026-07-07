079eeade0f44512ffda4ff2e86be7e831c25defb
Jiten Basnet <jiten.basnet@ebpearls.com>
2026-07-07 13:47:24 +0545

    :up: update apple and google pay to deno compatible

---
+++ constants.deno.js
@@ -0,0 +1,21 @@
+export var SERVICES = {
+	UNITY: 'unity',
+	APPLE: 'apple',
+	GOOGLE: 'google',
+	WINDOWS: 'windows',
+	AMAZON: 'amazon',
+	ROKU: 'roku',
+	FACEBOOK: 'facebook'
+};
+
+export var UNITY = {
+	APPLE: 'AppleAppStore',
+	GOOGLE: 'GooglePlay',
+	AMAZON: 'AmazonApps'
+};
+
+export var VALIDATION = {
+	SUCCESS: 0,
+	FAILURE: 1,
+	POSSIBLE_HACK: 2
+};
---
+++ mod.deno.js
@@ -0,0 +1,123 @@
+import * as apple from './lib/apple.deno.js';
+import * as googleApi from './lib/googleAPI.deno.js';
+import * as constants from './constants.deno.js';
+import * as verbose from './lib/verbose.deno.js';
+
+export var APPLE = constants.SERVICES.APPLE;
+export var GOOGLE = constants.SERVICES.GOOGLE;
+export var VALIDATION = constants.VALIDATION;
+
+var googleConfig = null;
+
+export function config(configIn) {
+    if (!configIn) {
+        return;
+    }
+    verbose.setup(configIn);
+    if (configIn.googleServiceAccount) {
+        googleConfig = configIn.googleServiceAccount;
+        googleApi.config(configIn.googleServiceAccount);
+    }
+    apple.readConfig(configIn);
+}
+
+export function setup() {
+    return new Promise(function (resolve, reject) {
+        apple.setup(function (error) {
+            if (error) return reject(error);
+            resolve();
+        });
+    });
+}
+
+export function validate(service, receipt) {
+    return new Promise(function (resolve, reject) {
+        switch (service) {
+            case APPLE:
+                apple.validatePurchase(null, receipt, function (error, data) {
+                    if (error) return reject(error);
+                    resolve(data);
+                });
+                break;
+            case GOOGLE:
+                googleApi.validatePurchase(null, receipt, function (error, data) {
+                    if (error) return reject(error);
+                    resolve(data);
+                });
+                break;
+            default:
+                reject(new Error('Invalid service: ' + service));
+        }
+    });
+}
+
+export function validateOnce(service, secretOrPubKey, receipt) {
+    return new Promise(function (resolve, reject) {
+        switch (service) {
+            case APPLE:
+                apple.validatePurchase(secretOrPubKey, receipt, function (error, data) {
+                    if (error) return reject(error);
+                    resolve(data);
+                });
+                break;
+            case GOOGLE:
+                googleApi.validatePurchase(secretOrPubKey, receipt, function (error, data) {
+                    if (error) return reject(error);
+                    resolve(data);
+                });
+                break;
+            default:
+                reject(new Error('Invalid service: ' + service));
+        }
+    });
+}
+
+export function isValidated(response) {
+    if (response && response.status === constants.VALIDATION.SUCCESS) {
+        return true;
+    }
+    return false;
+}
+
+export function isExpired(purchasedItem) {
+    if (!purchasedItem || !purchasedItem.transactionId) {
+        throw new Error('Invalid purchased item given:\n' + JSON.stringify(purchasedItem));
+    }
+    if (purchasedItem.cancellationDate) {
+        return true;
+    }
+    if (!purchasedItem.expirationDate) {
+        return false;
+    }
+    if (Date.now() - purchasedItem.expirationDate >= 0) {
+        return true;
+    }
+    return false;
+}
+
+export function isCanceled(purchasedItem) {
+    if (!purchasedItem || !purchasedItem.transactionId) {
+        throw new Error('Invalid purchased item given:\n' + JSON.stringify(purchasedItem));
+    }
+    if (purchasedItem.cancellationDate) {
+        return true;
+    }
+    return false;
+}
+
+export function getPurchaseData(purchaseData, options) {
+    if (!purchaseData || !purchaseData.service) {
+        return null;
+    }
+    switch (purchaseData.service) {
+        case APPLE:
+            return apple.getPurchaseData(purchaseData, options);
+        case GOOGLE:
+            if (googleApi.getPurchaseData) {
+                return googleApi.getPurchaseData(purchaseData, options);
+            }
+            return null;
+        default:
+            return null;
+    }
+}
